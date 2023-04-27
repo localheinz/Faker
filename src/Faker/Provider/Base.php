9f0946a5b556190e07e6f171165ce6d6e0c6beac
Andreas Möller <am@localheinz.com>
2023-04-27 17:18:56 +0200

    Fix: DocBlock

--- src/Faker/Provider/Base.php
+++ src/Faker/Provider/Base.php
@@ -179,9 +179,9 @@ class Base
     /**
      * Returns randomly ordered subsequence of $count elements from a provided array
      *
-     * @param array $array           Array to take elements from. Defaults to a-c
-     * @param int   $count           Number of elements to take.
-     * @param bool  $allowDuplicates Allow elements to be picked several times. Defaults to false
+     * @param array|\Traversable $array           Array to take elements from. Defaults to a-c
+     * @param int                $count           Number of elements to take.
+     * @param bool               $allowDuplicates Allow elements to be picked several times. Defaults to false
      *
      * @throws \LengthException When requesting more elements than provided
      *
